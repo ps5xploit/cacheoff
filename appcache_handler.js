ea4fd24b2d8e37e836b1dc9124fe972127f9c88f
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2024-08-07 20:58:06 +0200

    Update appcache_handler.js

--- appcache_handler.js
+++ appcache_handler.js
@@ -32,7 +32,7 @@ function add_cache_event_toasts() {
     var appCache = window.applicationCache;
     
     if (!navigator.onLine) {
-        showToast('Off-line');
+        showToast('Off-line wait...');
     }
 
     appCache.addEventListener('cached', function (e) {
