70c7d96431ee26f08d0c46642f1ad3e1029ee829
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2024-12-28 20:04:32 +0100

    Update payload_map.js

--- payload_map.js
+++ payload_map.js
@@ -3,7 +3,7 @@ const payload_map = [
     displayTitle: 'etaHEN',
     description: '', // Dejar "description" vacío
     info: 'Descripción de etaHEN', // Mantener "info"
-    fileName: 'etaHEN-1.9B.bin',
+    fileName: 'etaHEN-1.9B.bin.gz',
     author: 'LM',
     version: '?'
   },
