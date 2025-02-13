ca7601ebfd83feaab4411e3d00b00ba21192c3a9
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2025-02-13 21:40:00 +0100

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -1,77 +1,75 @@
 const payload_map = [
-
-    {
-        displayTitle: 'etaHEN',
-        description: '',
-        info: 'etaHEN 2.0b by LM',
-        fileName: 'etaHEN-20B.bin.gz',
-        author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
-        version: '2.0 beta'
-    },
+  {
+    displayTitle: 'etaHEN',
+    description: '', // Dejar "description" vacío
+    info: 'Descripción de etaHEN', // Mantener "info"
+    fileName: 'etaHEN-20B.bin',
+    author: 'LM',
+    version: '?'
+  },
+  {
+    displayTitle: 'etaHEN 1.8b',
+    description: '', // Dejar "description" vacío
+    info: 'Descripción de etaHEN', // Mantener "info"
+    fileName: 'etaHEN-18B.bin',
+    author: 'LM',
+    version: '1.8b'
+  },
+ 
+  {
+    displayTitle: 'HW Info',
+    description: '', // Dejar "description" vacío
+    info: 'Descripción de HW Info', // Mantener "info"
+    fileName: 'hwinfo-tornblom.elf',
+    author: '?',
+    source: '?',
+    version: '?'
+  },
+  {
+    displayTitle: 'Remove Cache',
+    description: '', // Dejar "description" vacío
+    info: 'Descripción de Remove Cache', // Mantener "info"
+    fileName: 'Browser_appCache_remove.elf',
+    author: 'Storm21CH',
+    version: '1.0fix'
+  },
+  {
+    displayTitle: 'Version',
+    description: '', // Dejar "description" vacío
+    info: 'Descripción de Versions', // Mantener "info"
+    fileName: 'versions.elf',
+    author: '?',
+    version: '1.0'
+  },
+  {
+    displayTitle: 'Fan control',
+    description: '', // Dejar "description" vacío
+    info: 'Descripción de Fan control', // Mantener "info"
+    fileName: 'fan_threshold.elf',
+    author: '?',
+    source: '?',
+    version: '1.0'
+  },
+   {
+            displayTitle: 'Backup DB',
+            description: '', // Dejar "description" vacío
+            fileName: 'BackupDB.elf',
+            info: 'Make a BACKUP from db to usb', // Mantener "info"
+            author: 'Make a BACKUP from db to usb',
+            source: 'Make a BACKUP from db to usb',
+            version: '1.1'
+        },
+  
 {
-        displayTitle: 'etaHEN 1.8b',
-        description: '',
-        info: 'etaHEN 1.8b by LM',
-        fileName: 'etaHEN-1.8B.bin.gz',
-        author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
-        version: '1.8 beta'
-    },
-    {
-        displayTitle: 'HW Info',
-        description: '',
-        info: 'Temperature information, frequency',
-        fileName: 'hwinfo-tornblom.elf',
-        author: '?',
-        source: '?',
-        version: '?'
-    },
-    {
-        displayTitle: 'ELF Loader',
-        description: '',
-        info: 'Uses port 9021. Persistent network elf loader',
-        fileName: 'elfldr.elf',
-        author: 'john-tornblom',
-        source:'https://github.com/ps5-payload-dev/elfldr/releases',
-        version: '0.14'
-    },
-    {
-        displayTitle: 'Web SRV',
-        description: '',
-        info: 'Uses elfldr. Custom homebrew loader Runs on port 8000',
-        fileName: 'websrv.elf',
-        author: 'john-tornblom',
-        loader: 'john-tornblom-elfldr',
-        source:'https://github.com/ps5-payload-dev/websrv/releases',
-        version: '0.4'
-    },
-    {
-        displayTitle: 'Versions',
-        description: '',
-        info: 'Shows kernel build, os and sdk versions',
-        fileName: 'versions.elf',
-        author: 'SiSTRo',
-        source:'https://github.com/SiSTR0/ps5-versions/releases/download/v1.0/ps5-versions.elf',
-        version: '1.0'
-    },
-    {
-        displayTitle: 'Cache Remove',
-        description: '',
-        info: 'Removes appcache from browser',
-        fileName: 'Browser_appCache_remove.elf',
-        author: 'Storm21CH',
-        source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
-        version: '1.0fix'
-    },
-    {
-        displayTitle: 'Fan control',
-        description: '',
-        info: 'Fan threshold set 60°',
-        fileName: 'fan_threshold.elf',
-        author: '?',
-        source: '?',
-        version: '1.0'
-    },
-  // {
+            displayTitle: 'ELF Loader',
+            description: '', // Dejar "description" vacío
+            fileName: 'elfldr.elf',
+            info: 'ELF Loader', // Mantener "info"
+            author: 'ELF Loader',
+            source: 'ELF Loader',
+            version: '1.1'
+        },
+// {
     //     displayTitle: 'ps5-welcome',
     //     description: '',
     //     info: 'ps5-welcome',
@@ -81,4 +79,17 @@ const payload_map = [
     // },
 ];
 
+// JavaScript para mostrar info en lugar de description
+const btns = document.querySelectorAll('.btn');
+const infoElement = document.getElementById('info'); // Obtén el elemento de info por su ID
 
+btns.forEach(btn => {
+  btn.addEventListener('mouseover', () => {
+    const info = btn.getAttribute('data-info');
+    infoElement.textContent = info; // Actualiza el contenido del elemento de info
+  });
+
+  btn.addEventListener('mouseout', () => {
+    infoElement.textContent = ''; // Limpia el contenido cuando el ratón sale del elemento
+  });
+});
